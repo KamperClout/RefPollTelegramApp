b90ed2d608123d0bc52f0a668fcd209ec936f218
martlet <jasperbarrette@gmail.com>
2024-07-12 21:06:28 +0400

    tut chto-to

--- resources/views/livewire/create-account/create-account.blade.php
+++ resources/views/livewire/create-account/create-account.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="text-blue-variant-gray-text">
-        hjhtrgyergt
+    <div class="bg-blue-variant-font max-w-screen-Android ">
+        <div class="col-auto gap-2">
+            <span class="text-black"> Создание </span>
+            <span class="text-black"> аккаунта </span>
+        </div>
+{{--        <form action="">--}}
+{{--        <div class="grid grid-cols-5 gap-3">--}}
+{{--            <div class="col-span-5">--}}
+{{--                <input type="text" class="">--}}
+{{--            </div>--}}
+
+{{--        </div>--}}
     </div>
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
-// module.exports = {
-//   theme: {
 //     screens: {
 //         'Android': '360px',
 //     },
-//       colors: {
-//         'blue-variant': {
-//             'main-font': '#E7EAF2',
-//             'font': '#B1B4BE',
-//             'button': '#EFEFEF',
-//             'gray-text': '#0047FF',
-//         }
-//     }
-//   },
-// }
-//
+
 module.exports = {
     content: [
         './resources/**/*.blade.php',
@@ -21,12 +9,18 @@ module.exports = {
         './resources/**/*.vue',
     ],
     theme: {
+        screens:{
+            'Android': '360px',
+        },
         extend: {
             colors: {
-                'blue-variant-main-font': '#E7EAF2',
-                'blue-variant-font': '#B1B4BE',
-                'blue-variant-button': '#EFEFEF',
-                'blue-variant-gray-text': '#0047FF',
+                'blue-variant': {
+                    'main-font': '#E7EAF2',
+                    'font': '#B1B4BE',
+                    'button': '#EFEFEF',
+                    'gray-text': '#A0A0A0',
+                    'blue-button': '#0047FF',
+                }
             },
         },
     },
