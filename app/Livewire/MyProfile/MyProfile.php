d7190ae53f3da009441c2df43a34b67a0dd56dec
LAPTOP-1Q0NKG6M\Serge <Sergey2018sar@yandex.ru>
2024-07-14 21:23:11 +0400

    add data in profile

--- app/Livewire/MyProfile/MyProfile.php
+++ app/Livewire/MyProfile/MyProfile.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire\MyProfile;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class MyProfile extends Component
 {
     public function render()
     {
-        return view('livewire.my-profile.my-profile');
+        $user = Auth::user();
+        $surname = $user->surname;
+        $name = mb_substr($user->name, 0, 1, 'UTF-8') . '.';;
+        $patronymic = mb_substr($user->patronymic, 0, 1, 'UTF-8') . '.';
+        $test_passed = $user->test_passed;
+        return view('livewire.my-profile.my-profile',['surname' => $surname,'name' => $name,'patronymic' => $patronymic,'test_passed' => $test_passed,]);
     }
 }
--- resources/views/livewire/my-profile/my-profile.blade.php
+++ resources/views/livewire/my-profile/my-profile.blade.php
@@ -1,3 +1,20 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+<div class="container mx-auto">
+    <h1 class="text-2xl font-bold text-blue-variant-gray-text">Welcome to your MyProfile!</h1>
+    <p class="mt-4 text-blue-variant-font">This is the my-profile page.</p>
+    <div>
+        <div>Мой</div>
+        <div>Профиль</div>
+        <div>{{$surname . ' ' . $name . ' ' . $patronymic}}</div>
+        @if ($test_passed)
+            <div>
+                Тест сдан
+            </div>
+            <img src="#"/>
+        @else
+            <div>
+                Тест не сдан
+            </div>
+            <img src="#"/>
+        @endif
+    </div>
 </div>
