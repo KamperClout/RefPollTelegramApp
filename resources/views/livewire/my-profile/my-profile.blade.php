305cbec5f009b89a48136aeabe11f3016cf07c29
LAPTOP-1Q0NKG6M\Serge <Sergey2018sar@yandex.ru>
2024-07-16 01:42:36 +0400

    add links

--- resources/views/livewire/my-profile/my-profile.blade.php
+++ resources/views/livewire/my-profile/my-profile.blade.php
@@ -41,38 +41,38 @@
             @endif
         </div>
         <div class="mt-[8px] w-[344px] h-[252px] bg-white rounded-[28px] border-[1px] border-sc-border flex flex-col">
-            <div class="flex flex-row w-[328px] h-[56px] rounded-[20px] bg-white border-[1px] border-sc-border ml-[8px] mt-[8px] pl-[8px] pt-[8px]">
+            <a href="#" class="flex flex-row w-[328px] h-[56px] rounded-[20px] bg-white border-[1px] border-sc-border ml-[8px] mt-[8px] pl-[8px] pt-[8px]">
                 <div class="w-[40x] h-[40px] rounded-[12px] bg-white border-[1px] border-sc-border p-[12px]">
                     <div class="w-[16px] h-[16px] bg-[url('/images/book.png')]">
                         <button class=""></button>
                     </div>
                 </div>
                 <span class="ml-[16px] mt-[13px] font-[13px] font-Montserrat font-medium text-sc-almost-black"> Пройти обучение </span>
-            </div>
-            <div class="flex flex-row w-[328px] h-[56px] rounded-[20px] bg-white border-[1px] border-sc-border ml-[8px] mt-[4px] pl-[8px] pt-[8px]">
+            </a>
+            <a href="#" class="flex flex-row w-[328px] h-[56px] rounded-[20px] bg-white border-[1px] border-sc-border ml-[8px] mt-[4px] pl-[8px] pt-[8px]">
                 <div class="w-[40x] h-[40px] rounded-[12px] bg-white border-[1px] border-sc-border p-[12px]">
                     <div class="w-[16px] h-[16px] bg-[url('/images/clipboard-tick.png')]">
                         <button class=""></button>
                     </div>
                 </div>
                 <span class="ml-[16px] mt-[13px] font-[13px] font-Montserrat font-medium text-sc-almost-black"> Пройти тест </span>
-            </div>
-            <div class="flex flex-row w-[328px] h-[56px] rounded-[20px] bg-white border-[1px] border-sc-border ml-[8px] mt-[4px] pl-[8px] pt-[8px]">
+            </a>
+            <a href="#" class="flex flex-row w-[328px] h-[56px] rounded-[20px] bg-white border-[1px] border-sc-border ml-[8px] mt-[4px] pl-[8px] pt-[8px]">
                 <div class="w-[40x] h-[40px] rounded-[12px] bg-white border-[1px] border-sc-border p-[12px]">
                     <div class="w-[16px] h-[16px] bg-[url('/images/document-text.png')]">
                         <button class=""></button>
                     </div>
                 </div>
                 <span class="ml-[16px] mt-[13px] font-[13px] font-Montserrat font-medium text-sc-almost-black"> Реквизиты самозанятого </span>
-            </div>
-            <div class="flex flex-row w-[328px] h-[56px] rounded-[20px] bg-white border-[1px] border-sc-border ml-[8px] mt-[4px] pl-[8px] pt-[8px]">
+            </a>
+            <a href="#" class="flex flex-row w-[328px] h-[56px] rounded-[20px] bg-white border-[1px] border-sc-border ml-[8px] mt-[4px] pl-[8px] pt-[8px]">
                 <div class="w-[40x] h-[40px] rounded-[12px] bg-white border-[1px] border-sc-border p-[12px]">
                     <div class="w-[16px] h-[16px] bg-[url('/images/headphone.png')]">
                         <button class=""></button>
                     </div>
                 </div>
                 <span class="ml-[16px] mt-[13px] font-[13px] font-Montserrat font-medium text-sc-almost-black"> Связаться с поддержкой </span>
-            </div>
+            </a>
         </div>
     </div>
 </div>
