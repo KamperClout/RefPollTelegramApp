a529e8d13b2c652e95d2d6ed71bc5aa6c9ccdc75
martlet <jasperbarrette@gmail.com>
2024-07-16 21:33:56 +0400

    my-clients

--- resources/views/livewire/my-clients/my-clients.blade.php
+++ resources/views/livewire/my-clients/my-clients.blade.php
@@ -1,24 +1,41 @@
-<div class="container mx-auto">
+<div class="big-div">
     @if (!$showForm)
-        <h1 class="text-2xl font-bold text-blue-variant-gray-text">Welcome to your MyClients!</h1>
-        <p class="mt-4 text-blue-variant-font">This is the my-clients page.</p>
-        <div>
-            <p>Всего оплативших: {{ $paidCount }}</p>
-            <p>Всего не оплативших: {{ $unpaidCount }}</p>
-        </div>
-        <div class="mt-4">
-            <input type="text" wire:model.lazy="search" class="border rounded p-2" placeholder=""/>
-        </div>
-        @foreach($clients as $client)
-            @if (!$client->is_payment)
-                <div>
-                    {{ $loop->iteration }}. {{ $client->name }} {{ $client->surname }} {{ $client->patronymic }} - {{ $client->phone }}
+        <div class="w-[344px] h-[212px] bg-white bg-opacity-80 rounded-[28px] border-[1px] border-sc-border flex flex-col mt-[26px]">
+            <div class="flex flex-col mt-[32px] ml-[24px]">
+                <span class="text-base text-[16px] font-medium font-MontserratBold text-sc-almost-black"> Мои </span>
+                <span class="font-semibold font-MontserratBold text-[24px] text-sc-almost-black"> Клиенты </span>
+            </div>
+            <div class="flex flex row">
+                <div class="w-[160px] h-[103px] bg-sc-check rounded-[20px] ml-[8px] mt-[20px] border-sc-check border-[2px] flex flex-col">
+                    <span class="w-[97px] h-[13px] font-medium font-Montserrat text-white text-[14px] ml-[17px] mt-[10px]"> Оплатившие </span>
+                    <span class="font-Montserrat text-white text-[36px] ml-[17px] mt-[3px]"> {{ $paidCount }} </span>
                 </div>
-            @endif
-        @endforeach
-        <button wire:click="openForm">
-            +
-        </button>
+                <div class="w-[160px] h-[103px] bg-sc-white rounded-[20px] ml-[8px] mt-[20px] border-sc-form border-[2px] flex flex-col">
+                    <span class="w-[97px] h-[13px] font-medium font-Montserrat text-sc-almost-black text-[14px] ml-[17px] mt-[10px]">Всего</span>
+                    <span class="font-Montserrat text-sc-check text-[36px] ml-[17px] mt-[3px]">{{ $unpaidCount }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="w-[344px] h-[450px] bg-white bg-opacity-80 rounded-[20px] border-[1px] border-sc-border flex flex-col mt-[8px]">
+            <div class="fixed top-[332px] left-[29px] w-[16px] h-[16px] bg-[url('/images/search-normal.png')]"> </div>
+            <input type="text" wire:model.lazy="search" class="w-[328px] h-[40px] border rounded-[12px] mt-[8px] ml-[8px] bg-sc-form-back mb-[4px]" placeholder=""/>
+            @foreach($clients as $client)
+                @if (!$client->is_payment)
+                    <div class="w-[328px] h-[64px] bg-white rounded-[20px] mt-[4px] border-[1px] border-sc-border border-opacity-80 ml-[8px] flex flex-row">
+                        <div class="w-[48px] h-[48px] bg-white rounded-[12px] border-[1px] border-sc-border ml-[8px] mt-[8px] pl-[17px] pt-[10px]">
+                            {{ $loop->iteration }}
+                        </div>
+                        <div class="flex flex-col ml-[16px] mt-[9px]">
+                            <span class="text-sc-almost-black text-[13px] font-Montserrat font-medium"> {{ $client->surname}} {{ $client->name }}</span>
+                            <span class="text-sc-check">  {{ $client->phone }} </span>
+                        </div>
+                    </div>
+                @endif
+            @endforeach
+            <button wire:click="openForm" class="w-[64px] h-[64px] rounded-[20px] fixed top-[720px] left-[280px] bg-sc-yellow text-sc-almost-black text-[36px]">
+                +
+            </button>
+        </div>
     @else
         @if (session()->has('success'))
             <div class="bg-green-200 text-green-800 p-2 mb-4 rounded">
